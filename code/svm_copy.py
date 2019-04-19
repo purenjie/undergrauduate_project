3e06351d0cb556e64a1dec876002d49bff62b17c
solejay <prj960827@gmail.com>
2019-04-19 16:43:58 +0800

    写入预测率和均方误差到日志文件

--- code/svm_copy.py
+++ code/svm_copy.py
@@ -3,43 +3,44 @@ from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
-# 读取 excel 文件，默认返回第一张表
+# 读取 excel 文件，默认返回第一张表 
+# 返回类型：<class 'pandas.core.frame.DataFrame'>
 def get_data(file):
     sheet = pd.read_excel(io=file)
     return sheet
 
-# 输出模型预测率
-def score(svr_model, x, y):
-    svr_model.fit(x, y)
-    print('预测率：', svr_model.score(x, y))
+# 输出模型预测率 并写入日志文件
+def write_log(svr_model, x_test, y_test, excel_file):
 
-# 传入  输入 输出 核函数 
-# 画图  输出 预测输出
-def plot_graph(x, y, svr_model):
-    sample = [i for i in range(1, len(x)+1)]
-    sample = np.reshape(sample, (len(sample), 1))
-    svr_model.fit(x, y)
+    pre_rate = svr_model.score(x_test, y_test)
+    print('预测率：%.4f' % pre_rate)
 
-    # 实线图——实际数据
-    plt.plot(sample, y, color='black', label='Data')
+    y_pred = svr_rbf.predict(x_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print('均方误差：%.4f' % mse) 
 
-    # 实线图——预测数据
-    plt.plot(sample, svr_model.predict(x), color='red', label='RBF model')
+    log_file = '/home/solejay/program/undergrauduate_project/log.txt'
+    with open(log_file, 'a') as f:
+        s0 = '预测率：%.4f' % pre_rate + '\n'
+        s1 = '均方误差：%.4f' % mse + '\n'
+        s2 = '读取文件：' + excel_file.split('/')[-1] + '\n'
+        s3 = '模型参数：' + str(svr_model) + '\n'
+        s4 = '=============================================================\n'
+        s = s0 + s1 + s2 + s3 + s4
+        f.write(s)
 
-    plt.xlabel('sample')
-    plt.ylabel('utilization')
-    plt.title('Support Vector Regression')
-    plt.legend()
-    plt.show()
 
-# 测试集 实际输出 和 预测输出
-def plot_result(y_test, y_pre_test):
+# 画出预测值和实际值的图像
+def plot_graph(svr_model, x_test, y_test):
     sample = [i for i in range(1, len(y_test)+1)]
     sample = np.reshape(sample, (len(sample), 1))
     
-    plt.scatter(sample, y_test, color='black', label='y_test')
-    plt.plot(sample, y_pre_test, color='red', label='y_pre_test')
+    y_pred = svr_rbf.predict(x_test)
+
+    plt.plot(sample, y_test, color='black', label='y_test')
+    plt.plot(sample, y_pred, color='red', label='y_pred')
 
     plt.xlabel('sample')
     plt.ylabel('utilization')
@@ -47,37 +48,21 @@ def plot_result(y_test, y_pre_test):
     plt.legend()
     plt.show()
 
-file = '/home/solejay/program/bishe/learn/less.xlsx'
-data = get_data(file)
-# print(type(data))  # <class 'pandas.core.frame.DataFrame'>
-x_train, x_test, y_train, y_test = train_test_split(data.iloc[:, 0:5], data.iloc[:, 5], test_size=0.25, random_state=33)
 
-x = data.iloc[:, 0:5]
-y = data.iloc[:, 5]
-# print(x)
-# print(y)
-# print(type(x))  # <class 'pandas.core.frame.DataFrame'>
-# print(type(y))  # <class 'pandas.core.series.Series'>
+if __name__ == "__main__":
 
-svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-svr_rbf.fit(x_train, y_train)
-y_pre_test = svr_rbf.predict(x_test)
-# print(type(np.array(y_pre_test)))  # <class 'numpy.ndarray'>
-# print(type(np.array(y_test)))  # <class 'numpy.ndarray'>
+    excel_file = '/home/solejay/program/undergrauduate_project/excel/more.xlsx'
+    data = get_data(excel_file)
 
-def rmse(predictions, targets):
-    return np.sqrt(((predictions - targets) ** 2).mean())
+    # 划分训练集和测试集
+    x_train, x_test, y_train, y_test = train_test_split(data.iloc[:, 0:5], data.iloc[:, 5], test_size=0.25, random_state=33)
 
-rmse_val = rmse(np.array(y_pre_test), np.array(y_test))
-print('均方差：', rmse_val)
+    # 核函数
+    svr_rbf = SVR(kernel='rbf', gamma='auto')
+    svr_rbf.fit(x_train, y_train)
+
+    write_log(svr_rbf, x_test, y_test, excel_file)
+    plot_graph(svr_rbf, x_test, y_test)
 
 
-
-
-
-# plot_graph(x, y, svr_rbf)
-# score(svr_rbf, x, y)
-# score(svr_rbf, x_train, y_train)
-# score(svr_rbf, x_test, y_test)
-# plot_result(y_test, y_pre_test)
 
--- code/svr.py
+++ code/svr.py
@@ -3,6 +3,7 @@ from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 # 读取 excel 文件，默认返回第一张表 
 # 返回类型：<class 'pandas.core.frame.DataFrame'>
@@ -10,67 +11,37 @@ def get_data(file):
     sheet = pd.read_excel(io=file)
     return sheet
 
-# 输出模型预测率
-def score(svr_model, x, y):
-    svr_model.fit(x, y)
-    print('预测率：', svr_model.score(x, y))
+# 输出模型预测率 并写入日志文件
+def write_log(svr_model, x_test, y_test, excel_file):
 
-# 传入  输入 输出 核函数 
-# 画图  输出 预测输出
-def plot_graph(x, y, svr_model):
-    # 样本数——横轴
-    sample = [i for i in range(1, len(x)+1)]
-    sample = np.reshape(sample, (len(sample), 1))
+    pre_rate = svr_model.score(x_test, y_test)
+    print('预测率：%.4f' % pre_rate)
 
-    # 根据给定的训练数据拟合SVM模型
-    svr_model.fit(x, y)
+    y_pred = svr_rbf.predict(x_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print('均方误差：%.4f' % mse) 
 
-    
-    plt.plot(sample, y, color='black', label='Data')  # 实际数据
-    plt.plot(sample, svr_model.predict(x), color='red', label='RBF model')  # 预测数据
+    log_file = '/home/solejay/program/undergrauduate_project/log.txt'
+    with open(log_file, 'a') as f:
+        s0 = '预测率：%.4f' % pre_rate + '\n'
+        s1 = '均方误差：%.4f' % mse + '\n'
+        s2 = '读取文件：' + excel_file.split('/')[-1] + '\n'
+        s3 = '模型参数：' + str(svr_model) + '\n'
+        s4 = '=============================================================\n'
+        s = s0 + s1 + s2 + s3 + s4
+        f.write(s)
 
-    plt.xlabel('sample')  # x 轴标签
-    plt.ylabel('utilization')  # y 轴标签
-    plt.title('Support Vector Regression')  # 图像标题
-    plt.legend()  # 显示图例（label）
-    plt.show()  # 显示图像
 
-# 测试集 实际输出 和 预测输出
-def plot_result(y_test, y_pre_test):
+# 画出预测值和实际值的图像
+def plot_graph(svr_model, x_test, y_test):
     sample = [i for i in range(1, len(y_test)+1)]
     sample = np.reshape(sample, (len(sample), 1))
     
+    y_pred = svr_rbf.predict(x_test)
+
     plt.plot(sample, y_test, color='black', label='y_test')
-    plt.plot(sample, y_pre_test, color='red', label='y_pre_test')
+    plt.plot(sample, y_pred, color='red', label='y_pred')
 
-    plt.xlabel('sample')
-    plt.ylabel('utilization')
-    plt.title('Support Vector Regression')
-    plt.legend()
-    plt.show()
-
-def plot_three_kernel(x, y):
-    sample = [i for i in range(1, len(x)+1)]
-    sample = np.reshape(sample, (len(sample), 1))
-
-    # 核函数
-    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)  # defining the support vector regression models
-    svr_lin = SVR(kernel='linear', C=1e3, gamma='auto')
-    # svr_poly = SVR(kernel='poly', C=1e3, degree=2, gamma='auto')  # 效果太差
-
-    # 拟合输入数据
-    svr_rbf.fit(x, y)  # fitting the data points in the models
-    svr_lin.fit(x, y)
-    # svr_poly.fit(x, y)  # poly 核函数显示不出图像
-
-    # 散点图——实际数据
-    plt.scatter(sample, y, color='black', label='Data')
-
-    # 实线图——预测数据
-    plt.plot(sample, svr_rbf.predict(x), color='red', label='RBF model')
-    plt.plot(sample, svr_lin.predict(x), color='blue', label='Linear model')
-    # plt.plot(sample, svr_poly.predict(x), color='yellow', label='Poly model')
-    
     plt.xlabel('sample')
     plt.ylabel('utilization')
     plt.title('Support Vector Regression')
@@ -80,24 +51,18 @@ def plot_three_kernel(x, y):
 
 if __name__ == "__main__":
 
-    file = '/home/solejay/program/bishe/learn/more.xlsx'
-    data = get_data(file)
+    excel_file = '/home/solejay/program/undergrauduate_project/excel/more.xlsx'
+    data = get_data(excel_file)
 
-    # 整体集
-    x = data.iloc[:, 0:5]
-    y = data.iloc[:, 5]
-
-    # 训练集和测试集
+    # 划分训练集和测试集
     x_train, x_test, y_train, y_test = train_test_split(data.iloc[:, 0:5], data.iloc[:, 5], test_size=0.25, random_state=33)
 
     # 核函数
-    svr_rbf = SVR(kernel='rbf', C=1e3, gamma='auto')
-    svr_lin = SVR(kernel='linear', C=1e3, gamma='auto')
-    svr_poly = SVR(kernel='poly', C=1e3, degree=2, gamma='auto')
-
-    # 测试集预测结果图
+    svr_rbf = SVR(kernel='rbf', gamma='auto')
     svr_rbf.fit(x_train, y_train)
-    y_pre_test = svr_rbf.predict(x_test)
-    plot_result(y_test, y_pre_test)
+
+    write_log(svr_rbf, x_test, y_test, excel_file)
+    plot_graph(svr_rbf, x_test, y_test)
+
 
 
